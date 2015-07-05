7056f9ed8a37463fa9231c9da876d18470308942
mika <example.com>
2015-07-05 15:17:58 +0900

    複数の一時ファイルにソートしたものを入れた

--- large_sort.c
+++ large_sort.c
@@ -13,60 +13,31 @@ int main(int argc, char** argv){
   char* fileName = argv[1];
   FILE* fp = fopen(fileName, "rb");
   FILE* fpW = fopen("result", "ab");
-  FILE* fpT;
+  FILE* fpT[3];
+  int k;
+  for(k = 0; k < 3; k++){
+    fpT[k] = tmpfile();
+  }
   FILE fileArray[10];
   int num = 1000 * 1000 / sizeof(int); //約1MB分
   int* buffer = (int*)malloc(num * sizeof(int));
   int* bufferW = (int*)malloc(num * sizeof(int) * 2);
-  int isTemp = 0;
+  int numTemp = 0;
   while (1) {
     size_t ret = fread(buffer, sizeof(int), num, fp);
     if (ret == 0) {
       break;
     }
     qsort(buffer, ret, sizeof(int), (int(*)(const void*, const void*))int_cmp);
-    //ここで一時ファイルを作成
-    if(isTemp != 0){
-      int* buffer2 = (int*)malloc(num * sizeof(int));
-      size_t ret2 = fread(buffer2, sizeof(int), num, fpT);
-      int i = 0, j = 0, count = 0;
-      while(i < ret && j < ret2){
-	if(buffer[i] < buffer2[j]){
-	  bufferW[count++] = buffer[i++];
-	}
-	else{
-	  bufferW[count++] = buffer2[j++];
-	}
-      }
-      int k;
-      if(i != ret){
-	for(k = i; k < ret; k++){
-	  bufferW[count++] = buffer[k];
-	}
-      }
-      else{
-	for(k = j; j < ret2; j++){
-	  bufferW[count++] = buffer2[k];
-	}
-      }
-      fwrite(bufferW, sizeof(int), ret+ret2, fpW);
-    }
-    fpT = tmpfile();
-
-    size_t test = fwrite(buffer, sizeof(int), ret, fpT);
-    rewind(fpT);
-    isTemp = 1;
-    /*
-    fread(buffer, sizeof(int), ret, fpT);
-    int i;
-    for(i = 0; i < ret; i++){
-      printf("%d\n", buffer[i]);
-    }
-    */
-    //fileArray[count++] = fpT;
+    fwrite(buffer, sizeof(int), ret, fpT[numTemp++]);
   }
-
-  //size_t ret = fwrite(buffer, sizeof(int), num, fpW);
+  int i, j;
+  for(i = 0; i < numTemp; i++){
+    rewind(fpT[i]);
+    size_t ret = fread(buffer, sizeof(int), num, fpT[i]);
+    for(j = 0; j < num; j++)
+      printf("b: %d\n", buffer[j]);
+  }
   free(buffer);
   fclose(fp);
   fclose(fpW);
